9c6caff792266bfc47730be8dd5b013edd2f62cc
Ethan Tweitmann <ethantweitmann@gmail.com>
2022-03-03 09:29:14 -0800

    Build out structure of return method

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/Header.css';
+
+const Header = () => {
+  return (
+    <section className="Header">
+      <p>≡</p>
+      <h1>Referle</h1>
+      <div> 1 </div>
+    </section>
+  );
+}
+
+export default Header;
