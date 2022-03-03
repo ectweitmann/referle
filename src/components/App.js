783379a67e10270dcaacbd3d67d17056dd23b01a
Ethan Tweitmann <ethantweitmann@gmail.com>
2022-03-02 17:55:04 -0800

    Make class-based component, import Route and Switch

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import '../styles/App.css';
 
-function App() {
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      words: [],
+      error: ''
+    }
+  }
+
+  render = () => {
+    return (
+      <h1>Hello world</h1>
+    )
+  }
 }
+
 
 export default App;
