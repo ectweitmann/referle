ba9a2338ecd794e5a6c7a29c63c4c8c2d97b24f1
Ethan Tweitmann <ethantweitmann@gmail.com>
2022-03-03 08:40:32 -0800

    Build out componentDidMount method and state properties

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,16 +8,36 @@ class Home extends Component {
     super();
     this.state = {
       words: [],
-      pages: {},
-      error: '',
+      previousPage: null,
+      currentPage: 1,
+      nextPage: 2,
+      limit: 10,
+      error: ''
     }
   }
 
+  componentDidMount = () => {
+    fetch(`http://localhost:3010/api/v1/heuristics/sorted/avg_tile_score?page=${this.state.currentPage}&limit=${this.state.limit}`)
+      .then(response => response.json())
+      .then(results => {
+        return this.setState({
+          words: results.result,
+          previousPage: results.previous ? results.previous.page : null,
+          currentPage: results.current.page,
+          nextPage: results.next ? results.next.page : null,
+        });
+      })
+      .catch(err => this.setState({ error: err.message }));
+  }
+
   render = () => {
-    <section className={"Home"}>
-      <WordsContainer words={this.state.words} />
-      <PageNav pages={this.state.pages} />
-    </section>
+    return (
+      <section className={"Home"}>
+        <h2>Home Page</h2>
+        {/* <WordsContainer words={this.state.words} /> */}
+        {/* <PageNav pages={this.state.pages} /> */}
+      </section>
+    );
   }
 }
 
