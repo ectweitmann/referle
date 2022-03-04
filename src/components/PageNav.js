46b75eb442bcbe9415587075bcd9dcb83cd4c97a
Ethan Tweitmann <ethantweitmann@gmail.com>
2022-03-04 12:24:19 -0800

    Refactor improper spacing

--- src/components/PageNav.js
+++ src/components/PageNav.js
@@ -2,16 +2,16 @@ import React from 'react';
 import '../styles/PageNav.css';
 
 const PageNav = ({ previous, current, next, changePage }) => {
-  const handleClickPrev = previous ? changePage: () => {};
+  const handleClickPrev = previous ? changePage : () => {};
   const handleClickNext = next ? changePage : () => {};
   return (
     <section className="page-nav">
       <div>
-        <p onClick={() => handleClickPrev(previous) } >‹ Prev.</p>
+        <p onClick={() => handleClickPrev(previous)}>‹ Prev.</p>
         <div className="page">
           <p className="page-number">{current}</p>
         </div>
-        <p onClick={() => handleClickNext(next)} >Next ›</p>
+        <p onClick={() => handleClickNext(next)}>Next ›</p>
       </div>
     </section>
   );
