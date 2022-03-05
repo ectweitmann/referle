7e047cc0445bc6ec2d71893e6733825a4a34ac26
Ethan Tweitmann <ethantweitmann@gmail.com>
2022-03-05 14:26:51 -0800

    Create handleResponse helper function

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,11 @@
+export const handleResponse = (response) => {
+  if (!response.ok) {
+    throw new Error(`${response.status} ${response.statusText}: Unable to load content. Please try reloading the page.`);
+  } else {
+    return response.json();
+  }
+}
+
 export const cleanData = (data) => {
   const { previous, current, next } = cleanPageData(data.previous, data.current, data.next);
   const result = cleanWordData(data.result);
