11c43e64648d501b9be4fb4f45de60f334322556
Ethan Tweitmann <ethantweitmann@gmail.com>
2022-03-04 17:53:03 -0800

    Convert into a class component

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from './Header';
+import NavModal from './NavModal';
 import Home from './Home';
 import WordBank from './WordBank';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import '../styles/App.css';
 
-const App = () => {
-  return (
-    <div className="App">
-      <Header />
-      <Switch>
-        <Route exact path="/" render= { () => <Home /> } />
-        <Route exact path="/wordbank" render= { () => <WordBank /> } />
-        <Redirect to="/" />
-      </Switch>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      showModal: false
+    }
+  }
+
+  toggleNavModal = () => {
+    this.setState(prevState => {
+      return { showModal: !prevState.showModal }
+    });
+  }
+
+  render = () => {
+    return (
+      <div className="App">
+        <Header toggleNavModal={this.toggleNavModal} />
+        <NavModal showModal={this.state.showModal} toggleNavModal={this.toggleNavModal} />
+        <Switch>
+          <Route exact path="/home" render= { () => <Home /> } />
+          <Route exact path="/wordbank" render= { () => <WordBank /> } />
+          <Redirect to="/home" />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 
