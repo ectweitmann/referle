930e8c76ab713bb6dff0a6fd9fbddec2bce585be
Ethan Tweitmann <ethantweitmann@gmail.com>
2022-03-09 13:08:46 -0800

    Add light mode icon, and theme toggling functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import '../styles/Header.css';
 
-const Header = ({ toggleNavModal }) => {
+const Header = ({ toggleNavModal, toggleDarkMode }) => {
   return (
     <section className="Header">
       <div className="hamburger-menu">
         <p className="hamburger" onClick={(event) => toggleNavModal(event)}>≡</p>
       </div>
       <h1 className="referle-title">REFERLE</h1>
-      <div className="wordbank-count"></div>
+      <div className="theme-container">
+        <p className="theme-icon" onClick={() => toggleDarkMode()}>☀︎</p>
+      </div>
     </section>
   );
 }
