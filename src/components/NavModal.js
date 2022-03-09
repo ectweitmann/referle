5d2948d93b84c9639d32c30e1fe548322b9e542f
Ethan Tweitmann <ethantweitmann@gmail.com>
2022-03-09 13:09:46 -0800

    Add inDarkMode prop for handling page theming

--- src/components/NavModal.js
+++ src/components/NavModal.js
@@ -3,16 +3,16 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import '../styles/NavModal.css';
 
-const NavModal = ({ showModal, toggleNavModal }) => {
+const NavModal = ({ showModal, toggleNavModal, inDarkMode }) => {
   if (!showModal) {
     return null;
   }
-
+  const lightMode = inDarkMode ? '' : 'light-mode';
   return (
     <div className="NavModal" onClick={() => toggleNavModal()}>
-      <div className="modal-content" onClick={event => event.stopPropagation()}>
+      <div className={`modal-content ${lightMode}`} onClick={event => event.stopPropagation()}>
         <section className="modal-header">
-          <a className="close-modal" onClick={() => toggleNavModal()}>×</a>
+          <button className={`close-modal ${lightMode}`} onClick={() => toggleNavModal()}>×</button>
         </section>
         <section className="modal-body">
           <Link to="/home" className="nav-link" onClick={() => toggleNavModal()}>
